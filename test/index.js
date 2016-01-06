bf5bb4caedf1decd96fb41197b34ee374deb7dad
calidion@gmail.com <calidion@gmail.com>
2016-01-06 16:19:30 +0800

    enabled error

--- test/index.js
+++ test/index.js
@@ -1,29 +1,34 @@
 import assert from 'assert';
-import JSErrors from '../lib';
+import BaseError from '../lib';
 import path from 'path';
 import i18n from '../lib/i18n';
 
 describe('js-errors', function () {
   it('should be able to generator Errors!', function () {
-    var generator = new JSErrors.Generator();
-    var TimeError = generator.generate(['TIME', 'IS', 'not', 'ok'], 'hello', 'en-US');
+    var TimeError = new BaseError({
+      errors: ['TIME', 'IS', 'not', 'ok'],
+      message: 'hello',
+      locales: 'en-US',
+      i18n: i18n.get()
+    });
     try {
-      throw new TimeError();
+      throw TimeError;
     } catch (e) {
       assert.equal(true, e.code === 'TimeIsNotOk');
       assert.equal(true, e.message === 'hello');
     }
-    console.log(__dirname);
     var dir = path.resolve(__dirname + '/locales');
-    console.log(dir);
-    generator = new JSErrors.Generator('zh-CN', dir);
-    TimeError = generator.generate(['TIME1', 'IS1', 'not2', 'ok1']);
+    TimeError = new BaseError({
+      errors: ['TIME1', 'IS1', 'not2', 'ok1'],
+      locales: 'zh-CN',
+      i18n: i18n.get(dir)
+    });
     try {
-      throw new TimeError();
+      throw TimeError;
     } catch (e) {
-      var json = e.restfy();
-      assert.equal(true, e.code === 'Time1Is1Not2Ok1');
-      assert.equal(true, json.code === 'Time1Is1Not2Ok1');
+      var json = e.restify();
+      assert.equal(true, e.name === 'Time1Is1Not2Ok1');
+      assert.equal(true, json.name === 'Time1Is1Not2Ok1');
       assert.equal(true, e.message === '');
       assert.equal(true, json.message === '');
     }
@@ -33,33 +38,39 @@ describe('js-errors', function () {
   it('should get predefined messages', function () {
     var errors = i18n.get();
 
-    var generator = new JSErrors.Generator();
-    var AnError = generator.generate(['ERROR']);
+    var AnError = new BaseError({
+      errors: ['ERROR']
+    });
     try {
-      throw new AnError();
+      throw AnError;
     } catch (e) {
-      console.log(e);
       assert.equal(true, e.message === '');
-      assert.equal(true, e.code === 'Error');
+      assert.equal(true, e.name === 'Error');
     }
 
     var dir = path.resolve(__dirname + '/locales');
     var errors1 = i18n.get(dir);
-    var generator1 = new JSErrors.Generator('en-US', dir);
-    var AnError1 = generator1.generate(['error']);
+
+    var AnError1 = new BaseError({
+      errors: ['error'],
+      locale: 'en-US',
+      i18n: i18n.get(dir)
+    });
     try {
-      throw new AnError1();
+      throw AnError1;
     } catch (e) {
-      console.log(e);
-      console.log(e.message);
       assert.equal(true, e.message === 'CError!');
       assert.equal(true, e.message === errors1['en-US'].Error.message);
     }
+    var AnError2 =
 
-    var generator2 = new JSErrors.Generator('en-US');
-    var AnError2 = generator2.generate(['user', 'not', 'found']);
+    new BaseError({
+      errors: ['user', 'not', 'found'],
+      locale: 'en-US',
+      i18n: i18n.get()
+    });
     try {
-      throw new AnError2();
+      throw AnError2;
     } catch (e) {
       console.log(e);
       console.log(e.message);
@@ -70,28 +81,59 @@ describe('js-errors', function () {
 
 
   it('should get prefixed messages', function () {
-    var generator = new JSErrors.Generator(null, null, null, 'sun:');
-    var AnError = generator.generate(['ERROR']);
+    var AnError =
+    new BaseError({
+      errors: ['ERROR'],
+      prefix: 'sun:'
+    });
     try {
-      throw new AnError();
+      throw AnError;
     } catch (e) {
       console.log(e);
       assert.equal(true, e.message === 'sun:');
-      assert.equal(true, e.code === 'Error');
+      assert.equal(true, e.name === 'Error');
     }
-
     var dir = path.resolve(__dirname + '/locales');
     var errors1 = i18n.get(dir);
-    var generator1 = new JSErrors.Generator('en-US', dir, null, 'google:');
 
-    var AnError1 = generator1.generate(['error']);
+    var AnError1 =
+    new BaseError({
+      errors: ['error'],
+      prefix: 'google:',
+      locale: 'en-US',
+      i18n: i18n.get(dir)
+    });
     try {
-      throw new AnError1();
+      throw AnError1;
     } catch (e) {
       console.log(e);
       console.log(e.message);
       assert.equal(true, e.message === 'google:CError!');
       assert.equal(true, e.message === 'google:' + errors1['en-US'].Error.message);
+    }
+
+    var AnError2 =
+      new BaseError({
+        errors: [],
+        prefix: 'google:',
+        locale: 'en-US',
+        i18n: {}
+      });
+    try {
+      throw AnError2;
+    } catch (e) {
+      console.log(e);
+      console.log(e.message);
+      assert.equal(true, e.message === 'google:');
+    }
+
+    var AnError3 =
+      new BaseError();
+    try {
+      throw AnError3;
+    } catch (e) {
+      console.log(e);
+      assert.equal(true, e.message === '');
     }
   });
 });
