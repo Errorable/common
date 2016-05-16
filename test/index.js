24b4db42dba2b9ff73c8659bcc97e676c61ea294
calidion <calidion@gmail.com>
2016-05-17 02:27:52 +0800

    matched web-errors code

--- test/index.js
+++ test/index.js
@@ -1,6 +1,15 @@
 import assert from 'assert';
 import errorable from 'errorable';
 import data from '../lib';
+import {errors} from 'web-errors';
+
+// console.log(errors);
+var extracted = {};
+for(var k in errors) {
+  var key = k.replace(/_/g, '');
+  extracted[key] = errors[k];
+}
+// console.log(extracted);
 
 var Generator = errorable.Generator;
 
@@ -12,5 +21,16 @@ describe('Generator', function () {
     assert.equal(true, generator.errors.Success.name === 'Success');
     assert.equal(true, generator.errors.InputInvalid.name === 'InputInvalid');
     generator.save('./lib/data/errors.json');
+  });
+
+  it('Should generate identical code to web errors', function () {
+    var generator = new Generator(data, 'zh-CN');
+    for(var k1 in generator.errors) {
+      var error = generator.errors[k1];
+      var key1 = k1.toUpperCase();
+      if (extracted[key1]) {
+        assert(extracted[key1].code === error.code);
+      }
+    }
   });
 });
